4c4bd96e905f31a50d0054aec378dd38ea0a58d2
GabbyVenturini <gabbyventurini@gmail.com>
2024-01-25 17:35:34 -0300

    Criado endpoint buscar livro

--- Biblioteca/src/main/java/com/biblioteca/controller/LivroController.java
+++ Biblioteca/src/main/java/com/biblioteca/controller/LivroController.java
@@ -1,15 +1,26 @@
 package com.biblioteca.controller;
 
-import org.springframework.stereotype.Controller;
+import com.biblioteca.model.Livro;
+import com.biblioteca.service.LivroService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/livros")
 public class LivroController {
-    @GetMapping("/hello")
-    public String hello(){
-        return "Hello Word!";
+    
+    private LivroService livroService;
+
+    public LivroController(LivroService livroService) {
+        this.livroService = livroService;
+    }
+
+    @GetMapping("/{id}")
+    public Livro buscarLivro(@PathVariable UUID idLivro){
+        return livroService.buscarLivro(idLivro);
     }
 }
--- Biblioteca/src/main/java/com/biblioteca/service/LivroService.java
+++ Biblioteca/src/main/java/com/biblioteca/service/LivroService.java
@@ -1,7 +1,24 @@
 package com.biblioteca.service;
 
+import com.biblioteca.model.Livro;
+import com.biblioteca.repository.LivroRepository;
+import lombok.AllArgsConstructor;
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
+@AllArgsConstructor
 public class LivroService {
+
+    private LivroRepository livroRepository;
+    public Livro buscarLivro(UUID idLivro) {
+        var livro = livroRepository.findById(idLivro);
+        if(livro.isPresent()){
+            return livro.get();
+        }else{
+            throw new ObjectNotFoundException(idLivro, Livro.class.getSimpleName());
+        }
+    }
 }
